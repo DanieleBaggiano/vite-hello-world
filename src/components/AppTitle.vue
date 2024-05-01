188430b786ae421de1b54bf37b749aae2eaea33e
DanieleBaggiano <danielebaggiano@gmail.com>
2024-05-01 18:24:16 +0200

    fine esercizio + bonus

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,13 @@
+<script>
+import { ref } from 'vue'
+</script>
+
+<template>
+  <div>
+    <h1>La mia prima app con Vite!</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/banner.vue
@@ -0,0 +1,26 @@
+<script>
+import { ref } from 'vue'
+
+export default {
+    data() {
+        return {
+            imageName: "Moscardini-alla-Luciana.avif"
+        };
+    },
+    methods: {
+        getImagePath(img) {
+            return new URL(`../assets/${img}`, import.meta.url).href;
+        }
+    },
+};
+</script>
+
+<template>
+  <div>
+    <img :src="getImagePath(imageName)">
+  </div>
+</template>
+
+<style scoped>
+
+</style>
